3f86b506cab4486f798a0120919f6c18f584f888
Ramel maynucas <ramelmaynucas07@gmail.com>
2024-11-20 23:24:56 +0800

    Add files via upload

---
+++ Maynucas_store2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Maynucas_store2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("********************************************************");
+        System.out.println("*                 RAMEL'S COFFEE SHOP                  *");
+        System.out.println("********************************************************");
+        
+        System.out.println("+----------------+---------------------+---------------+");
+        System.out.println("|   ESPRESSO     |      REFRESHER      |      SIZE'S   |");
+        System.out.println("+----------------+---------------------+---------------+");
+        System.out.println("|  Machiato (70) | BLUEBERRY SODA  (70)| 8oz           |");
+        System.out.println("|   LATTE   (55) | YUZU SODA       (70)| 12oz (+5)     |");
+        System.out.println("|  AFFOGATO (45) | STRAWBERRY SODA (70)| 16oz (+10)    |");
+        System.out.println("|   MOCHA   (60) | HONEY SODA      (70)| 20oz (+15)    |");
+        System.out.println("|  MATCHAA  (50) | TEA LEMON SODA  (70)|               |");
+        System.out.println("| CAPPUCCINO(50) | BLACK TEA SODA  (70)|               |");
+        System.out.println("+----------------+---------------------+---------------+");
+    }
+    
+    
+    
+}
